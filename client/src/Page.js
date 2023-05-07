1adf27865c6086c1f10c279c8dd5e3b35b2177ae
sgarmendia <sgarmendia@gmail.com>
2023-05-07 13:54:38 +0200

    first commit

---
+++ client/src/Page.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Page = () => {
+    return (
+    <div>
+      <h1>This is another PAGE</h1>
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+export default Page;
