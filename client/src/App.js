0259500665ef76f1ece34c2897bb1c279451f778
sgarmendia <sgarmendia@gmail.com>
2023-05-10 22:10:11 +0200

    update header

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className='App-title'>Fib Calculator</h1>
+          <h1 className='App-title'>Fib Calculator in KUBERNETES</h1>
           <Link to="/">Home</Link>
           <Link to="/page">Page</Link>
         </header>
